7b5127c291cbc668f3b53b24a8a0f7db55d1758e
ShahareyarKhan <shahareyar2003@gmail.com>
2025-03-14 00:09:27 +0530

    commit

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,5 +1,16 @@
+// export async function GET(req: Request) {
+//     return new Response(JSON.stringify({ message: "Seed route working!" }), {
+//       status: 200,
+//     });
+//   }
+
+
+//   .next\server\app\page\app-build-manifest.jso
+export const runtime = "nodejs"; // Ensure it's using Node.js
+
+import bcrypt from "bcrypt";
+
 export async function GET(req: Request) {
-    return new Response(JSON.stringify({ message: "Seed route working!" }), {
-      status: 200,
-    });
-  }
+    const hashedPassword = await bcrypt.hash("password", 10);
+    return new Response(JSON.stringify({ hashedPassword }), { status: 200 });
+}
